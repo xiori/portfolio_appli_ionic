46763dd9cc52e05a4fe9a1861555220bd15d65dd
enzo34 <eclavelloux@gmail.com>
2019-04-06 13:42:08 +0200

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { HomeService } from './home.service';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  homeList: string[];
+  projetList: string[];
+  compList: string[];
+
+
+
+  private homeListeSubscription: Subscription;
+  private projetListSubscription: Subscription;
+  private compListSubscription: Subscription;
+
+  constructor(private homeService: HomeService){
+
+  }
+
+  ngOnInit(){
+    this.homeListeSubscription = this.homeService.homeResult.subscribe(homes => {
+      this.homeList = homes;
+    });
+    this.projetListSubscription = this.homeService.projetResult.subscribe(projets => {
+      this.projetList = projets;
+    });
+    this.compListSubscription = this.homeService.compResult.subscribe(competences => {
+      this.compList = competences;
+    });
+
+
+    this.homeService.fetchHomes();
+    this.homeService.fetchProjets();
+    this.homeService.fetchCompetences();
+  }
 
 }
